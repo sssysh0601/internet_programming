3957e51e4ea0043b13e48cc88c182fe5fcf2c155
ktj9701 <ktj9701@naver.com>
2022-04-29 18:22:29 +0900

    app.js

---
+++ src/App.js
@@ -0,0 +1,51 @@
+/*import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import { Component,useEffect } from 'react';
+import Start from "./Components/Start";
+import {
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+function App(){
+
+  return (
+<Route exact={true} path="/" component={Start} />
+  );
+
+}
+export default App;*/
+
+import React, { Component } from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Start from "./Components/Start";
+import {
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+export default class App extends Component {
+  state = {
+    id : "",
+  }
+
+  handleChange =(e)=>{
+    this.setState({
+      [e.target.name] : e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+<Route exact={true} path="/" component={Start} />
+
+      </div>
+    )
+  }
+}
+
+
