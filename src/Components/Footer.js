ff3218a85a505337c4c5be254a2b64dd24a67992
ktj9701 <ktj9701@naver.com>
2022-04-29 18:23:02 +0900

    꼬리글

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+import './Footer.css';
+
+
+class Footer extends Component{
+    render(){
+    return (
+<footer className="Footer">
+        <h1 className="Footer-text">
+        꼬리글임
+        </h1>
+</footer>
+    );
+    }
+}
+export default Footer;
