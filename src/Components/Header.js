f0785ad7e585a822cc48fda5077dca37a66dd273
ktj9701 <ktj9701@naver.com>
2022-05-06 18:48:02 +0900

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,19 +10,14 @@ function Header(props){
             <div className= "Title-contents">
             <div className="Logo">
                <span className="Logo-text">
-                   <img src={imgTitle} alt="title image"></img>
+                   <Link to="/"><img src={imgTitle} alt="title image"></img></Link>
                </span>
             </div>
                 <nav className="Title-nav">
                 <ul>
-<<<<<<< HEAD
-                <li className="Login-text"><Link to="/login">글쓰기</Link></li>
                         <li className="Join-text"><Link to="/signup">회원가입</Link></li>
-=======
                         <li><a href="#">My Page </a></li>
-                        <li className="Login-text"><a href="#">Log in</a></li>
-                        <li className="Join-text"><a href="#">Sign up</a></li>
->>>>>>> f3c67cf16bcd224e8de7b9961f0f69cb66eb251b
+
 
                 </ul>
                 </nav>
