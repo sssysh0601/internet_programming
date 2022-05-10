04611ef1e2e1c1b5f92b12e6b234c0adbf5887d7
ktj9701 <ktj9701@naver.com>
2022-05-10 16:51:59 +0900

    포스팅(임시)

---
+++ src/Components/Posting.js
@@ -0,0 +1,37 @@
+import React, { Component, useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import './Postpage.css'
+function Posting({match}) {
+    const [text, settext]= useState([]);
+    const post_id =match.params.post_id;
+
+    useEffect(()=>{
+            const user = {
+              post_id: post_id
+            };
+      
+            fetch("http://localhost:3001/getposting", {
+               method: "post", // 통신방법
+               headers: {
+                  "content-type": "application/json",
+               },
+               body: JSON.stringify(user),
+            })
+               .then((res) => res.json())
+               .then((json) => {
+                settext(json)
+               });
+            
+    },[])
+ 
+return(
+    <><div dangerouslySetInnerHTML={ {__html: text.text}}>
+    </div><h1>{match.params.post_id}</h1></>
+)
+}
+    export default Posting;
+    
