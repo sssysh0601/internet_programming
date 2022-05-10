3b0f40223bdec64749aa375e8d12b8e0a84d15ba
ktj9701 <ktj9701@naver.com>
2022-05-10 16:52:33 +0900

    화면구성

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -1,18 +1,36 @@
-import React,{Component} from "react";
+import React, { useState, useEffect } from "react";
 import './Footer.css';
 import Header from "./Header";
 import Contents from "./contents";
 import Footer from "./Footer";
+import Logincontent from "./Logincontent";
 
-class Start extends Component{
-    render(){
+function Start(){
+  const [isLogin, setIsLogin] = useState(false)
+ 
+  useEffect(() => {
+    if(sessionStorage.getItem('user_id') === null){
+    // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 없다면
+      console.log('isLogin ?? :: ', isLogin)
+    } else {
+    // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 있다면
+    // 로그인 상태 변경
+      setIsLogin(true)
+      console.log('isLogin ?? :: ', isLogin)
+    }
+  })
+
     return (
         <div className="App">
         <Header />
-        <Contents />
+        {isLogin? 
+        <Logincontent isLogin={isLogin}/>
+        :
+        <Contents/>
+}
         <Footer />
       </div>
     )
-    }
+    
 }
 export default Start;
