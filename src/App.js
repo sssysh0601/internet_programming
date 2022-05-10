a33bbc7b399cf13c6526cea0ab67c63909a955f2
ktj9701 <ktj9701@naver.com>
2022-05-10 16:51:32 +0900

    App

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ import React, { Component } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Start from "./Components/Start";
-import login from "./Components/Login";
+import Postpage from "./Components/Postpage";
+import Posting from "./Components/Posting";
 import signup from "./Components/Signup";
 import {
   Switch,
@@ -44,8 +45,10 @@ export default class App extends Component {
     return (
       <div>
 <Route exact={true} path="/" component={Start} />
-<Route exact={true} path="/login" component={login} />
 <Route exact={true} path="/signup" component={signup} />
+<Route exact={true} path="/Postpage/:key" component={Postpage} />
+<Route exact={true} path="/Postpage/" component={Postpage} />
+<Route exact={true} path="/Posting/:post_id" component={Posting} />
       </div>
     )
   }
