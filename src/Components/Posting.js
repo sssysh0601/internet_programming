747ffbae13bb296894505c1e2c42550e9c2a5a41
ktj9701 <ktj9701@naver.com>
2022-05-27 20:09:58 +0900

    게시글 조회

--- src/Components/Posting.js
+++ src/Components/Posting.js
@@ -5,8 +5,30 @@ import Footer from "./Footer";
 import './Posting.css'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import { Link } from "react-router-dom";
+
+
+
 function Posting({ match }) {
 
+    function createData(URL) {
+        var slide = {
+           ImageURL: URL
+        }
+        return slide;
+     
+     };
+     
+     const rows = [
+        createData("/img1.jpg"),
+        createData("/img2.jpg"),
+        createData("/img3.jpg"),
+        createData("/img4.jpg"),
+        createData("/광고2.jpg"),
+        createData("/광고3.jpg"),
+     ]
+
+    const [image, setimage] = useState(rows[Math.floor(Math.random()*rows.length)].ImageURL);
     const [text, settext] = useState([]);
     const post_id = match.params.post_id;
     const [commend, setcommend] = useState("");
@@ -17,39 +39,43 @@ function Posting({ match }) {
     }
 
     const submit = () => {
-        alert( sessionStorage.getItem('user_id')+ commend +post_id)
-        const user = {
-            ID: sessionStorage.getItem('user_id'),
-            text: commend,
-            post_id: post_id,
-        };
+        alert(sessionStorage.getItem('user_id') + commend + post_id)
+        if (commend != "") {
+            const user = {
+                ID: sessionStorage.getItem('user_id'),
+                text: commend,
+                post_id: post_id,
+            };
 
-        fetch("http://localhost:3001/writecommend", {
+            fetch("http://localhost:3001/writecommend", {
+                method: "post", // 통신방법
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify(user),
+            })
+            window.location.replace(`/posting/${post_id}`)
+        }
+    };
+
+
+    const getcommend = () => {
+        const user = {
+            post_id: post_id
+        };
+        fetch("http://localhost:3001/getcommend", {
             method: "post", // 통신방법
             headers: {
                 "content-type": "application/json",
             },
             body: JSON.stringify(user),
         })
-        window.location.replace(`/posting/${post_id}`)
-    }
-const getcommend=()=>{
-    const user = {
-        post_id: post_id
-    };
-    fetch("http://localhost:3001/getcommend", {
-        method: "post", // 통신방법
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify(user),
-    })
-        .then((res) => res.json())
-        .then((json) => {
-            setcommends(json)
-        });
+            .then((res) => res.json())
+            .then((json) => {
+                setcommends(json)
+            });
 
-}
+    }
 
     useEffect(() => {
         const user = {
@@ -68,7 +94,8 @@ const getcommend=()=>{
                 settext(json)
             });
 
-            getcommend();
+        getcommend();
+        setimage(rows[Math.floor(Math.random()*rows.length)].ImageURL);
     }, [])
     return (
         < div className="App" >
@@ -89,31 +116,35 @@ const getcommend=()=>{
                                 </div>
                             </div>
                         </div>
-
+                        <div style={{ width:"100%" }}>
+                            <br/>
+                            <img src={image}></img>
+                        </div>                    
                         <div className="postingdiv" dangerouslySetInnerHTML={{ __html: text.text }}>
-
                         </div>
+
                     </div>
                     <div>
                         <div>
                             <input type="text" className="commendinput" onChange={handlercommend} />
-                            <input type="submit" value="댓글작성" onClick={submit} />
+                            <input type="submit" className="commendinputbtn" value="댓글작성" onClick={submit} />
+                            <Link to='/'><input type="button" className="commendinputbtn2" value="돌아가기"/></Link>
                         </div>
                         {commends.map(commends =>
-                        <div className="wrapComment">
-                            <div>
-                                <h6>작성자: {commends.ID}
-                                </h6>
+                            <div className="wrapComment">
+                                <div>
+                                    <h6>작성자: {commends.ID}
+                                    </h6>
+                                </div>
+                                <div style={{}}>
+                                    <h4>{commends.text}
+                                    </h4>
+                                </div>
+                                <div style={{}}>
+                                    <h6>{commends.commend_date}
+                                    </h6>
+                                </div>
                             </div>
-                            <div style={{ }}>
-                                <h4>{commends.text}
-                                </h4>
-                            </div>
-                            <div style={{ }}>
-                                <h6>{commends.commend_date}
-                                </h6>
-                            </div>
-                        </div>
                         )}
                     </div>
                 </div>
